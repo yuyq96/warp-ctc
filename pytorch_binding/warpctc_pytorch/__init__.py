70f24df2ec9abc404ca528825646e7b9939a3d37
Jiro Nishitoba <j.nshtb@gmail.com>
2018-08-19 18:29:22 +0900

    fix return value type of ctc.

--- pytorch_binding/warpctc_pytorch/__init__.py
+++ pytorch_binding/warpctc_pytorch/__init__.py
@@ -48,8 +48,7 @@ class _CTC(Function):
 
     @staticmethod
     def backward(ctx, grad_output):
-        return ctx.grads * grad_output.type_as(ctx.grads), None, None, None, None, None, None
-
+        return ctx.grads * grad_output.to(ctx.grads.device), None, None, None, None, None, None
 
 class CTCLoss(Module):
     """
