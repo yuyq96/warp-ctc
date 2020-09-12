bced748898754545dbcdfed6bd5647ae9f8b7cd5
Yusuke Nishioka <yusuke.nishioka@retrieva.jp>
2020-09-12 16:07:25 +0900

    Add /usr/local/cuda-10.2 to search paths to deal with libcublas problem when CUDA 10.1

--- pytorch_binding/setup.py
+++ pytorch_binding/setup.py
@@ -3,6 +3,7 @@ import platform
 import shutil
 import sys
 from setuptools import setup, find_packages
+from subprocess import Popen, PIPE
 
 from torch.utils.cpp_extension import BuildExtension, CppExtension
 import torch
@@ -36,8 +37,19 @@ shutil.copyfile(
 )
 
 
+def get_cuda_version():
+    proc = Popen(['nvcc', '--version'], stdout=PIPE, stderr=PIPE)
+    out, err = proc.communicate()
+    out.decode('utf-8').split('\n')[-2].split(', ')[-2].split(' ')
+    return out.decode('utf-8').split()[-2][:-1].replace('.', '')
+
+
 if torch.cuda.is_available() or "CUDA_HOME" in os.environ:
     enable_gpu = True
+    # For CUDA10.1, libcublas-10-2 is installed
+    # and we have to add /usr/local/cuda-10.2 to search paths
+    if get_cuda_version() == "101":
+        include_dirs.append("/usr/local/cuda-10.2/include")
 else:
     print("Torch was not built with CUDA support, not building warp-ctc GPU extensions.")
     enable_gpu = False
